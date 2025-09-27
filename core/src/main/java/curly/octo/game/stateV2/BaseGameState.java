0f5fed9c8ff8eda6517f1a4ea4386facde0783d0
Jason Cole <jaymcole2k@gmail.com>
2025-09-26 17:42:59 -0700

    Initial v2 game state classes

--- core/src/main/java/curly/octo/game/stateV2/BaseGameState.java
+++ core/src/main/java/curly/octo/game/stateV2/BaseGameState.java
@@ -1,6 +1,10 @@
 package curly.octo.game.stateV2;
 
 public abstract class BaseGameState {
+
+    protected State currentState;
+    protected SubState currentSubstate;
+
     public abstract void beginState(State previousState);
     public abstract void updateState(float delta);
     public abstract void endState(State nextState);
